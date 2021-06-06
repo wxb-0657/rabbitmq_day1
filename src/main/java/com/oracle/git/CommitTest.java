ac3e64f302b9cc60fcd5a0d472cf2995ceef6e3a
august <august@oracle.com>
2021-06-06 15:38:30 +0800

    third commit

--- src/main/java/com/oracle/git/CommitTest.java
+++ src/main/java/com/oracle/git/CommitTest.java
@@ -3,5 +3,9 @@ package com.oracle.git;
 public class CommitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git1!!");
+
+        System.out.println("hello git2!!");
+
     }
 }
